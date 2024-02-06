df480db0b2d05a7abde8f75f929a6749c0792e09
Anthony Hernandez <ahernandez94476@gmail.com>
2024-02-06 11:56:19 -0600

    this is the likedList

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 
-/*
-        implimentations
-append - inserts new element to end
-insert - inserts at index i
-sweep - cleans up memory
-display - prints every element in list
-*/
 struct node
 {
-    int data;   // list data
-    node *next; // ptr to next elm
-    node *last; // ptr to previous elm
+    int data;
+    node *next;
+    node *last;
 
     node(const int data)
     {
@@ -24,19 +17,20 @@ struct node
 //* - - - - - - - - - list class
 class linkedList
 {
-    node *head;     // first node
-    node *tail;     // last node
-    int numOfNodes; // keeps track of node num
+    node *head;
+    node *tail;
+    int numOfNodes;
 
 public:
-    void append(const int data);                      // adds new node to end
-    void insert(const int data, const int index = 0); // inserts new node at index
-    void sweep();                                     // deallocates memory starting at tail and ending at head
-    void display();                                   // displays stuff
-    void remove(const int i = 0);                     // removes at index i //* defaults i to 0  or default delete's head
-    int getNodeCount() { return numOfNodes; }         // returns the number of nodes
-    int getHead() { return head->data; }              // returns the data in head
-    int getTail() { return tail->data; }              // returns the data in tail
+    void append(const int data);
+    void insert(const int data, const int index = 0);
+    void sweep();
+    void display();
+    void remove(const int i = 0);
+    void removeAtTail();
+    int getNodeCount() { return numOfNodes; }
+    int getHead() { return head->data; }
+    int getTail() { return tail->data; }
     linkedList()
     {
         head = NULL;
@@ -48,9 +42,9 @@ public:
 // * - - - - - - - - - list methods
 void linkedList::append(const int data)
 {
-    tail = new node(data);       // new node = new tail
-    numOfNodes = numOfNodes + 1; // adds 1 to the node count
-    if (head == NULL)            // if there is not a head, assign one
+    tail = new node(data);
+    numOfNodes = numOfNodes + 1;
+    if (head == NULL)
     {
         head = tail;
         head->last = head;
@@ -58,19 +52,19 @@ void linkedList::append(const int data)
     }
     else
     {
-        node *lastElm = head->last; // inserts between tail and head (head's ptr to last = old tail)
-        tail->last = lastElm;       // new tail's ptr to last is assigned to old tail
-        lastElm->next = tail;       // old tail's ptr to next is assigned to new tail
-        tail->next = head;          // new tail's ptr to next = head
-        head->last = tail;          // head's ptr to last is assigned to new tail
+        node *lastElm = head->last;
+        tail->last = lastElm;
+        lastElm->next = tail;
+        tail->next = head;
+        head->last = tail;
     }
 }
 //* - - - - - insert method
 void linkedList::insert(const int data, int index)
 {
-    node *insertedNode = new node(data); // creates new node
-    numOfNodes = numOfNodes + 1;         // adds to node total
-    if (index == 0)                      // if insert at head, replace head
+    node *insertedNode = new node(data);
+    numOfNodes = numOfNodes + 1;
+    if (index == 0)
     {
         insertedNode->last = tail;
         insertedNode->next = head;
@@ -123,14 +117,14 @@ void linkedList::sweep()
 void linkedList::display()
 {
 
-    node *index = head; // starts at head
-    int i = 1;          // node num starts at 1
+    node *index = head;
+    int i = 1;
 
     while (index != tail)
     {
         std::cout << "the data for node" << i << " is :: \t" << index->data << std::endl;
-        i = i + 1;           // incriments node num
-        index = index->next; // new index is ptr to next node
+        i = i + 1;
+        index = index->next;
     }
 
     std::cout << "the data for last node is :: \t" << index->data << std::endl;
@@ -143,22 +137,13 @@ void linkedList::remove(int i)
     node *trash = head;
 
     int nodeTracker = 0;
+    // if there is a singular node
     if (numOfNodes == 1)
     {
         delete head;
         numOfNodes = numOfNodes - 1;
     }
-    else if (i == -1)
-    {
-        trash = tail;
-        tail = tail->last;
-        trash->next = NULL;
-        trash->last = NULL;
-        delete trash;
-        trash = nullptr;
-        head->last = tail;
-        tail->next = head;
-    }
+    // if removal is happening at tail
     else
     {
         while (nodeTracker != i)
@@ -180,4 +165,16 @@ void linkedList::remove(int i)
         trash = nullptr;
         numOfNodes = numOfNodes - 1;
     }
+}
+void linkedList::removeAtTail()
+{
+    node *trash;
+    trash = tail;
+    tail = tail->last;
+    trash->next = NULL;
+    trash->last = NULL;
+    delete trash;
+    trash = nullptr;
+    head->last = tail;
+    tail->next = head;
 }
