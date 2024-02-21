41c02921d4b9900d588e686b00991e2ae1334fb4
Anthony Hernandez <ahernandez94476@gmail.com>
2024-02-21 14:31:01 -0600

    myQeue 1.0

--- myQeue.h
+++ myQeue.h
@@ -1,19 +1,133 @@
-/*
-        implimentations ::
-enqeue - removes first element
-deqeue - adds element to end
-peek - checks what value is at index 0 without popping it
-size - checks size of qeue
-isEmpty - checks if empty
-*/
+#ifndef M_Q
+#define M_Q
+
 class myQeue
 {
+        struct node
+        {
+                node *next;
+                node *prev;
+                int data;
+                node(int pData) : next(nullptr), prev(nullptr), data(pData) {}
+        };
+        node *head;
+        node *tail;
+        node *reversalHold;
+        int count;
 
 public:
-        void enqeue(){};
+        //* ------------ push and pop
+        void enqeue(int data){};
         void deqeue(){};
-        int peek(){};
-        int getSize(){};
-        bool isEmpty();
+        void clean();
+        void reverseDeq();
+        //* ----------- misc methods
+        int peek() { return head->data; }
+        int getSize() { return count; }
+        bool isEmpty()
+        {
+                if (count == 0)
+                {
+                        return true;
+                }
+                else
+                {
+                        return false;
+                }
+        }
         void display(){};
+        myQeue() : head(nullptr), tail(nullptr), reversalHold(nullptr), count(0) {}
+        ~myQeue()
+        {
+                if (reversalHold != nullptr)
+                {
+                        delete reversalHold;
+                        reversalHold = nullptr;
+                }
+                clean();
+        }
 };
+//* - - - - - - - push and pops
+// enqeue
+void myQeue::enqeue(int data)
+{
+        node *createdNode = new node(data);
+        if (head == nullptr)
+        {
+                head = createdNode;
+        }
+        else
+        {
+                tail->next = createdNode;
+                createdNode->prev = tail;
+        }
+        tail = createdNode;
+        count = count + 1;
+}
+// dequeu
+void myQeue::deqeue()
+{
+        // TODO :: add an exception block for if deqeue when qeue is empty
+        try
+        {
+                if (isEmpty() == true)
+                {
+                        throw std::out_of_range("there no nodes to deqeue");
+                }
+
+                if (reversalHold != nullptr)
+                {
+                        delete reversalHold;
+                        reversalHold = nullptr;
+                }
+                reversalHold = head;
+                head = head->next;
+                head->prev = nullptr;
+                count = count - 1;
+        }
+        catch (std::out_of_range &exe)
+        {
+                std::cout << exe.what();
+        }
+}
+void myQeue::reverseDeq()
+{
+        try
+        {
+                if (reversalHold == nullptr)
+                {
+                        throw "there is nothing to reverse";
+                }
+                head->prev = reversalHold;
+                reversalHold = nullptr;
+        }
+        catch (const char *exception)
+        {
+                std::cout << exception;
+        }
+}
+void myQeue::clean()
+{
+        while (head != nullptr)
+        {
+                head = head->next;
+                head->prev = tail;
+                tail->next = head;
+                delete (head->prev);
+        }
+        tail = nullptr;
+        head = nullptr;
+}
+
+//* - - - - - - - misc methods
+void myQeue::display()
+{
+        node *travel = head;
+        while (travel != nullptr)
+        {
+                std::cout << travel->data << std::endl;
+                travel = travel->next;
+        }
+}
+
+#endif
